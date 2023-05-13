27809e39cd9c63a7bc15e30c7438ae1ad8f8895e
jadiunr <jadiunr@gmail.com>
2023-05-13 18:33:20 +0900

    kasu gomi kuso fix

--- lib/WatchersEye/Collector/Twitter.pm
+++ lib/WatchersEye/Collector/Twitter.pm
@@ -28,20 +28,13 @@ has twitter => (is => 'ro', lazy => 1, default => sub {
     );
 });
 has timer => (is => 'rw');
+has initialized => (is => 'rw', default => 0);
 
 sub run {
     my $self = shift;
     my $cv = AnyEvent->condvar;
 
-    $self->statuses($self->twitter->user_timeline({
-        user_id => $self->target->{account_id},
-        tweet_mode  => 'extended',
-        include_ext_alt_text => 1,
-        include_rts => $self->target->{include_rts} // 1,
-    }));
-    $self->since_id($self->statuses->[0]{id});
-
-    say $self->target->{label}. ": Connected. interval=". $self->interval;
+    say $self->target->{label}. ": Twitter fetch interval=". $self->interval;
 
     my $t = AnyEvent->timer(
         after => $self->interval,
@@ -50,15 +43,36 @@ sub run {
             eval {
                 $self->statuses($self->twitter->user_timeline({
                     user_id => $self->target->{account_id},
-                    since_id => $self->since_id,
+                    ($self->since_id ? (since_id => $self->since_id) : ()),
                     tweet_mode  => 'extended',
                     include_ext_alt_text => 1,
                     include_rts => $self->target->{include_rts} // 1,
                 }));
 
+                unless ($self->initialized) {
+                    $self->since_id($self->statuses->[0]{id});
+                    $self->initialized(1);
+                    say $self->target->{label}. ': Initialized';
+                    return;
+                }
+
                 if (@{$self->statuses}) {
                     for my $status (@{$self->statuses}) {
-                        my $media_attachments = [map { +{ url => $_->{media_url_https} } } @{$status->{extended_entities}{media}}];
+                        my $media_attachments = [];
+                        my $media_array = $status->{extended_entities}{media};
+                        if ($media_array) {
+                            for my $media (@$media_array) {
+                                if ($media->{type} eq 'photo') {
+                                    push(@$media_attachments, { url => $media->{media_url_https} });
+                                } elsif ($media->{type} eq 'video') {
+                                    my $variants = $media->{video_info}{variants};
+                                    for (@$variants) { $_->{bitrate} = 0 unless $_->{bitrate} }
+                                    my $url = (sort { $b->{bitrate} <=> $a->{bitrate} } @$variants)[0]{url};
+                                    $url =~ s/\?.+//;
+                                    push(@$media_attachments, { url => $url });
+                                }
+                            }
+                        }
                         my $reply_user = $status->{in_reply_to_user_id};
                         my $reply_status = $status->{in_reply_to_status_id};
                         $status->{full_text} .= ($reply_user and $reply_status)
@@ -81,7 +95,7 @@ sub run {
                     $self->since_id($self->statuses->[0]{id});
                 }
             };
-            say $@ if $@;
+            say $self->target->{label}. ': '. $@ if $@;
         }
     );
 
