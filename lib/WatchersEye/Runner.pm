2ff8d66e662c3d88e995f1bc0712bec267522676
jadiunr <jadiunr@gmail.com>
2023-05-14 21:53:55 +0900

    add support multiprocesses

--- lib/WatchersEye/Runner.pm
+++ lib/WatchersEye/Runner.pm
@@ -1,10 +1,13 @@
 package WatchersEye::Runner;
 use Moo;
 use utf8;
+use POSIX;
+use Clone 'clone';
 use WatchersEye::Config;
 use WatchersEye::Collector;
 use WatchersEye::Publisher;
 use YAML::XS 'LoadFile';
+use Parallel::ForkManager;
 
 has publisher => (
     is => 'ro',
@@ -17,14 +20,23 @@ has publisher => (
 sub run {
     my $self = shift;
 
-    for my $target (@{$Config->{targets}}) {
-        WatchersEye::Collector->new(
-            target => $target,
-            cb => sub {
-                my $status = shift;
-                $self->publisher->publish($target, $status);
-            }
-        )->run;
+    my $pm = Parallel::ForkManager->new(4);
+    my $targets = clone($Config->{targets});
+    my $num_targets = scalar(@$targets);
+    my $num_targets_per_process = ceil($num_targets / 4);
+
+    while(my $targets_per_process = splice @$targets, 0, $num_targets_per_process) {
+        $pm->start and next;
+        for my $target (@$targets_per_process) {
+            WatchersEye::Collector->new(
+                target => $target,
+                cb => sub {
+                    my $status = shift;
+                    $self->publisher->publish($target, $status);
+                },
+            )->run;
+        }
+        $pm->finish;
     }
 
     AnyEvent->condvar->recv;
